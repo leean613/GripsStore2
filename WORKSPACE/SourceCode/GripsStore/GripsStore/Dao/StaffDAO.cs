964ef64831af3b3298e6619586cccc9a6995ae85
Tran Quoc Hoan <tranquochoan@glotechvn.com>
2019-12-23 16:45:48 +0900

    add Staff, StaffDAO.cs

---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Dao/StaffDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using GripsStore.Common;
+using GripsStore.Models;
+using MyLiblay;
+using Npgsql;
+using static GripsStore.Models.Staff;
+
+namespace GripsStore.Dao
+{
+    public class StaffDao
+    {
+        public StaffJSON CheckLogin(string staffCode, string password)
+        {
+            StaffJSON result = new StaffJSON
+            {
+                success = false
+            };
+            StringBuilder sbSQL = new StringBuilder();
+            try
+            {
+                using (NpgDB npgDB = Connection.DBConnect())
+                {
+                    sbSQL.AppendLine("SELECT staff.staffcd, staff.staffnm, dept.deptcd, dept.deptnm FROM m_staff staff");
+                    sbSQL.AppendLine("LEFT JOIN m_dept dept ON dept.deptcd = staff.staffdeptcd");
+                    sbSQL.AppendLine("WHERE staff.staffcd = :p_staffCode");
+                    sbSQL.AppendLine("AND staff.password = :p_password");
+                    sbSQL.AppendLine("AND staff.strdte <= CURRENT_DATE");
+                    sbSQL.AppendLine("AND staff.enddte >= CURRENT_DATE");
+
+                    npgDB.Command = sbSQL.ToString();
+                    npgDB.SetParams("p_staffCode", staffCode);
+                    npgDB.SetParams("p_password", password);
+                    using (NpgsqlDataReader rec = npgDB.Query())
+                    {
+                        if (rec.Read())
+                        {
+                            result.staff = new Staff(rec);
+                            result.success = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+    }
+}
---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Models/Staff.cs
@@ -0,0 +1,39 @@
+﻿using Npgsql;
+using MyLiblay;
+namespace GripsStore.Models
+{
+    public class Staff
+    {
+        public string staffCode;
+        public string staffName;
+        public string staffDeptCode;
+        public string staffDeptName;
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// ｺﾝｽﾄﾗｸﾀ
+        /// </summary>
+        public Staff()
+        {
+        }
+    
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// ｺﾝｽﾄﾗｸﾀ
+        /// </summary>
+        /// <param name="rec"></param>
+        public Staff(NpgsqlDataReader rec)
+        {
+            this.staffCode = NpgDB.getString(rec, "staffcd");
+            this.staffName = NpgDB.getString(rec, "staffnm");
+            this.staffDeptCode = NpgDB.getString(rec, "deptcd");
+            this.staffDeptName = NpgDB.getString(rec, "deptnm");
+        }
+
+        public class StaffJSON
+        {
+            public bool success;
+            public Staff staff;
+        }
+    }
+}
