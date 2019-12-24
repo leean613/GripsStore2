7806816f19338b16fad664d8876d8b4cd1074871
Tran Quoc Hoan <tranquochoan@glotechvn.com>
2019-12-24 14:51:23 +0900

    add App.cs

---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Dao/AppDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GripsStore.Common;
+using GripsStore.Models;
+using MyLiblay;
+using Npgsql;
+using static GripsStore.Models.App;
+
+namespace GripsStore.Dao
+{
+    public class AppDao
+    {
+        public AppJSON GetApps()
+        {
+            AppJSON result = new AppJSON
+            {
+                apps = new List<App>()
+            };
+            StringBuilder sbSQL = new StringBuilder();
+            try
+            {
+                using (NpgDB npgDB = Connection.DBConnect())
+                {
+                    sbSQL.AppendLine("SELECT app.appid, app.name, app.description, app.icon, apk.vercd, apk.vernm, apk.filenm");
+                    sbSQL.AppendLine("FROM mapp app");
+                    sbSQL.AppendLine("LEFT JOIN(SELECT apk.appid, apk.vercd, apk.vernm, apk.filenm");
+                    sbSQL.AppendLine("        FROM dapk apk");
+                    sbSQL.AppendLine("        INNER JOIN (SELECT appid, max(vercd) maxvercd FROM dapk GROUP BY appid) newleast");
+                    sbSQL.AppendLine("        ON apk.appid = newleast.appid AND apk.vercd = newleast.maxvercd) apk");
+                    sbSQL.AppendLine("ON apk.appid = app.appid");
+                    npgDB.Command = sbSQL.ToString();
+                    using (NpgsqlDataReader rec = npgDB.Query())
+                    {
+                        while (rec.Read())
+                        {
+                            App app = new App(rec);
+                            result.apps.Add(app);
+                        }
+                    }
+                    if(result.apps.Count > 0)
+                    {
+                        result.success = true; 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+    }
+}
---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Models/App.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using MyLiblay;
+using System.Collections.Generic;
+
+namespace GripsStore.Models
+{
+    public class App
+    {
+        public string appId;
+        public string name;
+        public string description;
+        public string icon;
+        public string verCd;
+        public string verNm;
+        public string fileNm;
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// ｺﾝｽﾄﾗｸﾀ
+        /// </summary>
+        public App()
+        {
+        }
+    
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// ｺﾝｽﾄﾗｸﾀ
+        /// </summary>
+        /// <param name="rec"></param>
+        public App(NpgsqlDataReader rec)
+        {
+            this.appId = NpgDB.getString(rec, "appid");
+            this.name = NpgDB.getString(rec, "name");
+            this.description = NpgDB.getString(rec, "description");
+            this.icon = NpgDB.getString(rec, "icon");
+            this.verCd = NpgDB.getString(rec, "vercd");
+            this.verNm = NpgDB.getString(rec, "vernm");
+            this.fileNm = NpgDB.getString(rec, "filenm");
+        }
+
+        public class AppJSON
+        {
+            public bool success = false;
+            public List<App> apps;
+        }
+    }
+}
