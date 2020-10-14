b5861e80be7de17e1585d7b1cb3684f5318e65b2
an <lytruongan613@gmail.com>
2020-10-14 17:08:53 +0700

    ui

---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Controllers/StaffController.cs
@@ -0,0 +1,103 @@
+﻿using GripsStore.Dao;
+using GripsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GripsStore.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            StaffDao staffDao = new StaffDao();
+            List<Staff> list = staffDao.GetListStaff();
+            ViewData["Staffs"] = list;
+            return View();
+        }
+
+        /
+        public ActionResult Details(string code)
+        {
+            StaffDao staffDao = new StaffDao();
+
+            List<Staff> list = staffDao.GetListStaff(code);
+            ViewData["Staffs"] = list;
+            return View();
+        }
+
+        // GET: Staff/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Staff/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Staff/Edit/5
+        public JsonResult Update(string staffCode, string appId, string appName)
+        {
+            StaffDao staffDao = new StaffDao();
+            Staff staff = new Staff();
+            {
+
+            };
+            return Json(staffDao.Update(staffCode, staff));
+        }
+
+        // POST: Staff/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Staff/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WORKSPACE/SourceCode/GripsStore/GripsStore/Scripts/Staff/detail.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(onStartUp);
+function onStartUp() {
+    $('#btn-delete').click(function () {
+        deleteApp();
+    });
+    $('#btn-edit').click(function () {
+        editApp();
+    });
+}
+
+function deleteApp() {
+
+}
+
+function editApp() {
+    var appId = $('#staffCode').text();
+    location.href = "/Staff/edit/?code=" + appId;
+}
